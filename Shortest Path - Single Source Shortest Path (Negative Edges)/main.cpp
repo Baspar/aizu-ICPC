565fcf240314ccbb44ea7c224323737c6f9ef41e
Baspar <bastien.laine@insa-rouen.fr>
2018-02-06 13:04:02 +0800

    [edit] Shortest Path - Single Source Shortest Path (Negative Edges) Change to Bellman-Ford

--- Shortest Path - Single Source Shortest Path (Negative Edges)/main.cpp
+++ Shortest Path - Single Source Shortest Path (Negative Edges)/main.cpp
@@ -9,39 +9,62 @@ typedef vector<vector<Link> > Graph;
 int main () {
     int V, E, r;
     cin >> V >> E >> r;
-    Graph g(V, vector<Link>(V));
 
-    // Set identity to 0
-    for(int i=0; i<V; i++)
-        g[i][i] = make_pair(0, true);
+    vector<pair<bool, int> > distances(V);
+    vector<pair<pair<int, int>, int> > edges;
+
+    // Init source to a distance of 0
+    distances[r] = make_pair(true, 0);
 
     // Set known edges
     for(int i=0; i<E; i++) {
         int s, t, d;
         cin >> s >> t >> d;
-        g[s][t] = make_pair(d, true);
+        edges.push_back(make_pair(make_pair(s, t), d));
     }
 
-    // Run Floyd-Warshall algorithm
-    for(int k=0; k<V; k++)
-        for(int i=0; i<V; i++)
-            for(int j=0; j<V; j++)
-                if (
-                        (g[i][k].second && g[k][j].second) &&
-                        (!g[i][j].second || g[i][j].first > g[i][k].first + g[k][j].first))
-                    g[i][j] = make_pair(g[i][k].first + g[k][j].first, true);
+    // Run Ford-Bellman
+    for(int _=0; _<V; _++)
+        for(int i=0; i<E; i++){
+            int v1 = edges[i].first.first;
+            int v2 = edges[i].first.second;
+            int d = edges[i].second;
 
-    // Check if any cycle
-    for(int i=0; i<V; i++)
-        if (g[r][i].second && g[i][i].first < 0) {
-            cout << "NEGATIVE CYCLE" << endl;
-            return 0;
+            if (distances[v1].first) {
+                // If distance to v1 is Inf, ignore
+                if (distances[v2].first) {
+                    // If distance is defined, then check
+                    if (distances[v2].second > distances[v1].second + d)
+                        distances[v2] = make_pair(true, distances[v1].second + d);
+                } else {
+                    // If distance to v2 is Inf, force set
+                    distances[v2] = make_pair(true, distances[v1].second + d);
+                }
+            }
         }
 
-    // Print output
-    for(int i=0; i<V; i++)
-        if (g[r][i].second)
-            cout << g[r][i].first << endl;
+    // Detect loop
+    for(int i=0; i<E; i++){
+        int v1 = edges[i].first.first;
+        int v2 = edges[i].first.second;
+        int d = edges[i].second;
+
+        if (distances[v1].first)
+            if (distances[v2].first)
+                if (distances[v2].second > distances[v1].second + d) {
+                    cout << "NEGATIVE CYCLE" << endl;
+                    return 0;
+                }
+    }
+
+    // Log
+    for(int i=0; i<V; i++) {
+        bool isDefined = distances[i].first;
+        int d = distances[i].second;
+
+        if (isDefined)
+            cout << d << endl;
         else
             cout << "INF" << endl;
+    }
 }
